bbb4f9d213bd23c23559e0bdfc0d93e7f3757ec7
HadatMTCH <hadatskywalker007@gmail.com>
2026-03-23 23:01:28 +0700

    Update routes for restaunt and menu item

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants do
+    resources :menu_items, only: [:index, :create]
+  end
+  resources :menu_items, only: [:update, :destroy]
 end
