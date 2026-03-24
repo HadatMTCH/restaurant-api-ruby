01104c6d329b7e7be91c51364a6c6b9a38947a91
HadatMTCH <hadatskywalker007@gmail.com>
2026-03-24 13:18:22 +0700

    Add authenticate request on application controller before accessing the descendant function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,33 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
 
   private
+
+  def authenticate_request
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+
+    if token.present?
+      begin
+        @decoded = jwt_decode(token)
+        @current_user = User.find(@decoded[:user_id])
+        raise JWT::DecodeError if @current_user.jti.present? && @current_user.jti != @decoded[:jti]
+      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+        @current_user = User.find_by(api_key: token)
+      end
+    end
+
+    render json: { error: "Unauthorized" }, status: :unauthorized unless @current_user
+  end
+
+  def jwt_decode(token)
+    decoded = JWT.decode(token, Rails.application.secret_key_base)[0]
+    HashWithIndifferentAccess.new decoded
+  end
 
   def record_not_found(exception)
     render json: { error: exception.message }, status: :not_found
